0f1ae6e0b60da28fa21c222375f80eb5c0820423
Y Paing <ye@y3p.io>
2025-02-11 19:57:18 -0500

    adding additional notes for 2342

--- completed/2342/index.js
+++ completed/2342/index.js
@@ -18,9 +18,30 @@ var maximumSum = function(nums) {
       if (numList.length < 2) {
         numList.push(n);
       } else {
-        numList.push(n);
-        numList.sort((a,b) => b - a);
-        numList.pop();
+        // numList.push(n);
+        // numList.sort((a,b) => b - a);
+        // numList.pop();
+
+        // Trying static comparison
+        const [n1, n2] = numList;
+        const n3 = n;
+
+        let largest = 0;
+        let secondLargest = 0;
+
+        if (n1 >= n2 && n1 >= n3) {
+          largest = n1;
+          secondLargest = Math.max(n2, n3);
+        } else if (n2 >= n1 && n2 >= n3) {
+          largest = n2;
+          secondLargest = Math.max(n1,n3);
+        } else if (n3 >= n1 && n3 >= n2) {
+          largest = n3;
+          secondLargest = Math.max(n1,n2);
+        }
+
+        numList[0] = largest;
+        numList[1] = secondLargest;
       }
 
       sumMap.set(digitSum, numList);
@@ -37,4 +58,32 @@ var maximumSum = function(nums) {
   });
 
   return maxCandidate;
+
+  // Trying brute force O(n^2)
+  // Timesout for Testcase #64
+  // let maxCandidate = -1;
+  // for (let i=0; i<nums.length; i++) {
+  //     for (let j=i+1; j<nums.length; j++) {
+  //         let currNum = nums[i];
+  //         let n1 = 0
+  //         while(currNum > 0) {
+  //             n1 += currNum % 10;
+  //             currNum = Math.floor(currNum/10);
+  //         }
+
+  //         currNum = nums[j]
+  //         let n2 = 0
+  //         while(currNum > 0) {
+  //             n2 += currNum % 10;
+  //             currNum = Math.floor(currNum/10);
+  //         }
+
+  //         if (n1 === n2) {
+  //             maxCandidate = Math.max(maxCandidate, nums[i] + nums[j]);
+  //         }
+
+  //     }
+  // }
+
+  // return maxCandidate;
 };
