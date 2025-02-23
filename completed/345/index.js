b2a3a15a21d77a9bd9eb50e150532d8f391de8c5
Y Paing <ye@y3p.io>
2025-02-23 01:28:35 -0500

    adding a two pointer approach for 345

--- completed/345/index.js
+++ completed/345/index.js
@@ -2,24 +2,45 @@
  * @param {string} s
  * @return {string}
  */
-const VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+const VOWELS = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'])
 var reverseVowels = function(s) {
   const sArr = s.split('');
-  let vowelList = []
+  // let vowelList = []
 
-  for (let i=0; i<sArr.length; i++) {
-    const c = sArr[i];
-    if (VOWELS.includes(c)) {
-      vowelList.push(c);
-      sArr[i] = '*'
+  // for (let i=0; i<sArr.length; i++) {
+  //     const c = sArr[i];
+  //     if (VOWELS.has(c)) {
+  //         vowelList.push(c);
+  //         sArr[i] = '*'
+  //     }
+  // }
+
+  // for (let i=0; i<sArr.length; i++) {
+  //     const c = sArr[i];
+  //     if (c === '*') {
+  //         const v = vowelList.pop();
+  //         sArr[i] = v
+  //     }
+  // }
+  let l=0;
+  let r = sArr.length - 1;
+
+  while (l < r) {
+    if (!VOWELS.has(sArr[l])) {
+      l++;
     }
-  }
 
-  for (let i=0; i<sArr.length; i++) {
-    const c = sArr[i];
-    if (c === '*') {
-      const v = vowelList.pop();
-      sArr[i] = v
+    if (!VOWELS.has(sArr[r])) {
+      r--;
+    }
+
+    if (VOWELS.has(sArr[l]) && VOWELS.has(sArr[r])) {
+      // SWAP!
+      const temp = sArr[l]
+      sArr[l] = sArr[r]
+      sArr[r] = temp
+      l++;
+      r--;
     }
   }
 
