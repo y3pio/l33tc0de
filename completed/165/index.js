0a6e3c6aae07592e90fba0d547a3501c2cad7f6c
Ye Paing <ye@y3p.io>
2025-09-23 16:05:00 -0400

    Simplifying 165
    Refactor version comparison logic to handle variable-length version strings.

--- completed/165/index.js
+++ completed/165/index.js
@@ -1,52 +1,23 @@
-console.clear();
+/**
+ * @param {string} version1
+ * @param {string} version2
+ * @return {number}
+ */
 
-// Should return 0
-const t1 = {
-  s1: '1.01',
-  s2: '1.001'
-};
-
-// Should return 0
-const t2 = {
-  s1: '1.0',
-  s2: '1.0.0'
-};
-
-// Should return -1
-const t3 = {
-  s1: '0.1',
-  s2: '1.1'
-};
-
-// TODO: current implementation only accounts for MAJOR.MINOR.PATCH
-// .PATCH can be any additional number of .'s
-const compareVersionNumbers = (version1, version2) => {
+var compareVersion = function(version1, version2) {
   const ver1Arr = version1.split('.').map(ver => parseInt(ver));
   const ver2Arr = version2.split('.').map(ver => parseInt(ver));
+  
+  const length = Math.max(ver1Arr.length, ver2Arr.length);
 
-  const ver1Obj = {
-    major: ver1Arr[0] || 0,
-    minor: ver1Arr[1] || 0,
-    patch: ver1Arr[2] || 0
-  }
+  for (let i=0; i<length; i++) {
+    const v1 = ver1Arr[i] || 0;
+    const v2 = ver2Arr[i] || 0;
 
-  const ver2Obj = {
-    major: ver2Arr[0] || 0,
-    minor: ver2Arr[1] || 0,
-    patch: ver2Arr[2] || 0
-  }
-
-  if (ver1Obj.major !== ver2Obj.major) {
-    return ver1Obj.major > ver2Obj.major ? 1 : -1;
-  } else if (ver1Obj.minor !== ver2Obj.minor) {
-    return ver1Obj.minor > ver2Obj.minor ? 1 : -1;
-  } else if (ver1Obj.patch !== ver2Obj.patch) {
-    return ver1Obj.patch > ver2Obj.patch ? 1 : -1;
+    if (v1 !== v2) {
+        return v1 > v2 ? 1 : -1
+    }
   }
 
   return 0;
-}
-
-console.log(compareVersionNumbers(t1.s1, t1.s2))
-console.log(compareVersionNumbers(t2.s1, t2.s2))
-console.log(compareVersionNumbers(t3.s1, t3.s2))
+};
